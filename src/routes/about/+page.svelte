7de511f61a8285e40d938d8114dcc52b6f8177ec
Munene001 <lawrencemunenex@gmail.com>
2024-11-11 04:09:41 +0300

    mobile view about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -71,7 +71,8 @@
   Curabitur vestibulum semper dolor, quis lacinia urna elementum vitae.
 </div>
 <div class = "lastsection">
-  <div class = "lasttext">In hac habitasse platea dictumst. Pellentesque viverra sem nec orci lacinia, in bibendum urna mollis. Quisque nunc lacus, varius vel leo a, pretium lobortis metus. Vivamus consectetur consequat justo. Sed interdum nunc ut tristique congue. Quisque maximus mauris et dui sagittis scelerisque.</div>
+  <div class = "lasttext">In hac habitasse platea dictumst. Pellentesque viverra sem nec orci lacinia, in bibendum urna mollis. Quisque nunc lacus, varius vel leo a, pretium lobortis metus. Vivamus consectetur consequat justo. Sed interdum nunc ut tristique congue. Quisque maximus mauris et dui sagittis scelerisque.<ul><li>Integer ullamcorper lorem ultrices orci faucibus consequat. Sed ut ultricies dolor, vel consectetur ex. In rutrum quam felis, feugiat ultricies magna fermentum sed.</li><li>Nam in imperdiet lorem. Nulla condimentum, arcu nec aliquam ullamcorper, massa risus posuere enim, quis lacinia enim neque faucibus mi.</li><li>Mauris molestie ante in neque scelerisque, imperdiet ultrices quam commodo. Suspendisse a tincidunt justo.
+  </li></ul>Aliquam erat volutpat. Morbi vel enim quis purus accumsan faucibus ac ac diam. Donec dapibus lacus sit amet risus ultrices, vitae fringilla sem vehicula. Cras pulvinar, arcu id vehicula ultrices, sapien augue ullamcorper leo, at auctor lacus lectus sed lectus. Aenean molestie euismod nibh, feugiat viverra justo consectetur et. Nullam lacinia tempus finibus. Aenean in ligula augue. Aenean in nisl tristique, ultricies sapien quis, mollis enim. Morbi aliquet risus velit, convallis faucibus nisl sagittis quis. Fusce nulla lorem, fermentum et interdum porta, hendrerit feugiat est. Aliquam vitae quam augue.</div>
   <div class="imagecontainerab"><enhanced:img src={cgImage} alt="lasy image" class="last-img">
   </enhanced:img></div>
   
@@ -209,6 +210,7 @@
     display: flex;
     flex-direction: row;
     padding: 0 13px;
+    border-bottom: 2px solid rgba(128, 128, 128, 0.2);
     
 
   }
@@ -235,6 +237,94 @@
    
 
   }
+  @media (max-width: 640px){
+    .aboutuno{
+      height: 124px;
+      box-sizing: border-box;
+    }
+    .background-img{
+      height: 124px;
+    }
+    .heading{
+      position: relative;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      padding-left: 13px;
+      width: calc(100% - 13px);
+      
+      
+    
+     
+      
+      
+    }
+    .minhead{
+      font-weight: 500;
+    }
+    .maxhead{
+      font-size: 30px;
+      line-height: 36px;
+      font-weight: 500;
+
+    }
+    
+    .aboutsec2{
+      display: flex;
+      flex-direction: column;
+      height: auto;
+    }
+    .sec2{
+      margin-bottom: 20px;
+      font-size: 15px;
+      line-height: 20px;
+      
+    }
+    .section3{
+      flex-direction: column;
+      padding: 0 13px;
+      margin-top: 29px;
+      box-sizing: border-box;
+      
+    }
+    .longpar{
+      margin-top: 40px;
+    }
+    .numero{
+      justify-content: start;
+    }
+    .title{
+      justify-content: flex-start;
+      align-self: start;
+      
+    }
+    .titlesec1{
+      margin-left: 0px;
+      align-self: flex-start;
+      margin-top: 10px;
+
+    }
+    .titlesec2{
+      font-size: 26px;
+      font-weight: 400;
+      white-space: normal;
+      line-height: 31px;
+      margin-bottom: 10px;
+      
+    }
+
+    .lastsection{
+      display: flex;
+      flex-direction: column-reverse;
+    }
+    .imagecontainerab{
+      height: 403px;
+      margin-left: 0px;
+      margin-bottom: 40px;
+    }
+
+  }
+  
 
 </style>
 
